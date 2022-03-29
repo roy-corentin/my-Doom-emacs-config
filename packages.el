2c22b2481a529014db3cb806b81aa4704bc53d1b
Corentin ROY <croy@pop-os.localdomain>
2022-03-29 15:08:56 +0200

    Update my config with new os

--- packages.el
+++ packages.el
@@ -1,4 +1,4 @@
-;
+;; -*- no-byte-compile: t; -*-
 ;;; $DOOMDIR/packages.el
 
 ;; To install a package with Doom you must declare them here and run 'doom sync'
@@ -8,9 +8,7 @@
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
-(use-package company-tabnine)
-(package! powerline)
-(package! solaire-mode :disable t)
+
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
