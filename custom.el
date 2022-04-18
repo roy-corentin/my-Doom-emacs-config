0b2eac1ca380835c6d74eb9cbc8ea144cfbae22e
Corentin Roy <corentin.roy@epitech.eu>
2022-04-18 06:49:40 +0200

    Add org mode config and update lsp-ui-sideline config

--- custom.el
+++ custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(python-black lsp-python-ms vterm lsp-mode)))
+ '(package-selected-packages
+   '(lsp-tailwindcss visual-fill-column visual-fill org-bullets company-irony-c-headers flycheck-irony irony-eldoc company-irony python-black lsp-python-ms vterm lsp-mode)))
 ;;  '(package-selected-packages
 ;;    '(projectile-rails projectile python-black lsp-python-ms vterm lsp-mode))
 ;;  '(safe-local-variable-values '((checkdoc-package-keywords-flag))))
