50b605776a12b44e0b5e9e52fd6381dc60a404b5
roy-corentin <corentin.roy@epitech.eu>
2023-03-05 09:56:28 +0100

    [UP] size org title

--- config.el
+++ config.el
@@ -244,14 +244,14 @@
                              (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
 
   ;; Set faces for heading levels
-  (dolist (face '((org-level-1 . 1.2)
-                  (org-level-2 . 1.1)
-                  (org-level-3 . 1.0)
+  (dolist (face '((org-level-1 . 1.6)
+                  (org-level-2 . 1.3)
+                  (org-level-3 . 1.1)
                   (org-level-4 . 1.0)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.1)
-                  (org-level-7 . 1.1)
-                  (org-level-8 . 1.1)))
+                  (org-level-5 . 1.0)
+                  (org-level-6 . 1.0)
+                  (org-level-7 . 1.0)
+                  (org-level-8 . 1.0)))
     (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
 
   ;; Ensure that anything that should be fixed-pitch in Org files appears that way
