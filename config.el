968725958da3b83d40f57c6e12520f3d7eb06581
Corentin Roy <croy@mesvaccins.net>
2023-03-09 11:04:12 +0100

    [ADD] crystalline lsp for crystal-mode

--- config.el
+++ config.el
@@ -477,6 +477,17 @@
 (map! :leader
       :desc "Toggle Centaur Tabs" "t a" #'centaur-tabs-mode)
 
+;; (require 'magit-stats)
+
+(with-eval-after-load 'lsp-mode
+  (add-to-list 'lsp-language-id-configuration
+               '(crystal-mode . "crystal"))
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-stdio-connection '("crystalline"))
+                    :activation-fn (lsp-activate-on "crystal")
+                    :priority '1
+                    :server-id 'crystalline)))
+
 (setq +format-on-save-enabled-modes
       '(not web-mode))
 
