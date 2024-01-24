5a8635f5769ae0f54bd7a89ac77ae550683b16a1
roy-corentin <corentin.roy@epitech.eu>
2024-01-24 19:22:44 +0100

    [ADD] tab config to copy in doom files to fix emacsclient tabs

---
+++ tabs_config.el
@@ -0,0 +1,35 @@
+;;; ui/tabs/config.el -*- lexical-binding: t; -*-
+
+(use-package! centaur-tabs
+  :hook (server-after-make-frame . centaur-tabs-mode)
+  :init
+  (setq centaur-tabs-set-icons t
+        centaur-tabs-gray-out-icons 'buffer
+        centaur-tabs-set-bar 'left
+        centaur-tabs-set-modified-marker t
+        centaur-tabs-close-button "✕"
+        centaur-tabs-modified-marker "•"
+        centaur-tabs-icon-type 'nerd-icons
+        ;; Scrolling (with the mouse wheel) past the end of the tab list
+        ;; replaces the tab list with that of another Doom workspace. This
+        ;; prevents that.
+        centaur-tabs-cycle-scope 'tabs)
+
+  :config
+  (add-hook! '(+doom-dashboard-mode-hook +popup-buffer-mode-hook)
+    (defun +tabs-disable-centaur-tabs-mode-maybe-h ()
+      "Disable `centaur-tabs-mode' in current buffer."
+      (when (centaur-tabs-mode-on-p)
+        (centaur-tabs-local-mode)))))
+
+;; When started in daemon mode, centaur tabs does not work at all, so here is a fix
+(if (not (daemonp))
+    (centaur-tabs-mode)
+
+  (defun centaur-tabs--daemon-mode (frame)
+    (unless (and (featurep 'centaur-tabs) (centaur-tabs-mode-on-p))
+      (run-at-time nil nil (lambda () (centaur-tabs-mode)))))
+  (add-hook 'after-make-frame-functions #'centaur-tabs--daemon-mode))
+
+;; TODO tab-bar-mode (emacs 27)
+;; TODO tab-line-mode (emacs 27)
