555d6a34f13b0bedf00548831ea1ad1ccba7dec0
roy-corentin <corentin.roy@epitech.eu>
2024-01-24 19:30:16 +0100

    [DEL] useless custom config

--- custom.el
+++ custom.el
@@ -3,13 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(auth-source-save-behavior nil)
- '(custom-safe-themes
-   '("014cb63097fc7dbda3edf53eb09802237961cbb4c9e9abd705f23b86511b0a69" "8a379e7ac3a57e64de672dd744d4730b3bdb88ae328e8106f95cd81cbd44e0b6" "2035a16494e06636134de6d572ec47c30e26c3447eafeb6d3a9e8aee73732396" "88f7ee5594021c60a4a6a1c275614103de8c1435d6d08cc58882f920e0cec65e" "93011fe35859772a6766df8a4be817add8bfe105246173206478a0706f88b33d" default))
- '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages
-   '(spell-fu olivetti xeft treemacs-nerd-icons nerd-icons-dired docker-tramp inf-crystal lsp-tailwindcss magit-stats org-inline-anim))
- '(warning-suppress-log-types '((lsp-mode) (defvaralias))))
+   '(htmlize spell-fu olivetti xeft treemacs-nerd-icons nerd-icons-dired docker-tramp lsp-tailwindcss magit-stats org-inline-anim)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -19,8 +14,4 @@
  '(company-tooltip-annotation-selection ((t (:foreground "#ffd100"))))
  '(company-tooltip-common ((t (:foreground "#c3ac43"))))
  '(company-tooltip-common-selection ((t (:foreground "#ffd100"))))
- '(font-lock-comment-face ((t (:slant italic))))
- '(font-lock-function-name-face ((t (:slant italic))))
- '(font-lock-keyword-face ((t (:slant italic))))
- '(font-lock-variable-name-face (nil :foreground "#dcaeea" :slant 'italic))
  '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
