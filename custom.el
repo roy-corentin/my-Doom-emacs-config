217da93d99812f879dc31c6fdd21b98effeb93d6
roy-corentin <corentin.roy02@laposte.net>
2024-03-15 16:14:42 +0100

    [ADD] safe theme

--- custom.el
+++ custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(auth-source-save-behavior nil)
  '(custom-safe-themes
-   '("2035a16494e06636134de6d572ec47c30e26c3447eafeb6d3a9e8aee73732396" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" default))
+   '("8d3ef5ff6273f2a552152c7febc40eabca26bae05bd12bc85062e2dc224cde9a" "2035a16494e06636134de6d572ec47c30e26c3447eafeb6d3a9e8aee73732396" "e3daa8f18440301f3e54f2093fe15f4fe951986a8628e98dcd781efbec7a46f2" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -16,5 +16,8 @@
  '(company-tooltip-annotation-selection ((t (:foreground "#ffd100"))))
  '(company-tooltip-common ((t (:foreground "#c3ac43"))))
  '(company-tooltip-common-selection ((t (:foreground "#ffd100"))))
+ '(company-tooltip-scrollbar-thumb ((t (:background "white"))))
+ '(company-tooltip-scrollbar-track ((t (:background "gray10"))))
  '(font-lock-comment-face (nil :slant 'italic))
+ '(font-lock-variable-name-face (nil :slant 'italic))
  '(ts-fold-replacement-face ((t (:foreground unspecified :box nil :inherit font-lock-comment-face :weight light)))))
