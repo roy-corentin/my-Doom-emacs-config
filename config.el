89e87b387249f01dc3b5437ed4bb94589d0e10ce
roy-corentin <corentin.roy02@laposte.net>
2024-03-23 19:16:06 +0100

    [UP] go back to olivetti default style

--- config.el
+++ config.el
@@ -77,8 +77,7 @@
 (map! "C-M-k" #'drag-stuff-up)
 (map! "C-M-j" #'drag-stuff-down)
 
-(setq! olivetti-body-width 120
-       olivetti-style 'fancy)
+(setq! olivetti-body-width 120)
 
 (map! :leader
       :desc "Toggle Olivetti Mode" "t o" #'olivetti-mode)
