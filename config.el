db1489d35e5370d7dddcece5423d4d127fad8ea4
roy-corentin <corentin.roy02@laposte.net>
2024-03-23 11:49:30 +0100

    [DEL] duplicate code

--- config.el
+++ config.el
@@ -563,6 +563,9 @@
               ("C-s-TAB" . 'copilot-accept-completion-by-word)
               ("C-s-<tab>" . 'copilot-accept-completion-by-word)))
 
+(use-package elixir-ts-mode
+    :ensure t)
+
 (use-package! blamer
   :bind (("s-i" . blamer-show-posframe-commit-info))
   :defer 20
