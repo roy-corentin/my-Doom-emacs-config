ab744486128c31b6fccc768880104ce3286b424d
roy-corentin <corentin.roy02@laposte.net>
2024-03-23 19:08:46 +0100

    [UP] roam configuration + add new template for datastructure note

--- config.el
+++ config.el
@@ -383,19 +383,27 @@
 (after! org
   (add-hook 'after-revert-hook #'org-agenda-auto-refresh-agenda-buffer))
 
-(after! org
-  :ensure-t
+(use-package! org-roam
+  :ensure t
   :custom
-  (setq org-roam-directory "~/Dropbox/RoamNotes")
-  (setq org-roam-index-file "~/Dropbox/RoamNotes/index.org")
-  (setq org-roam-capture-templates `(("d" " Default" plain "\n\n* %?"
-                                      :icon ("nf-fa-file_text_o" :set "faicon" :color "lcyan")
-                                      :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
-                                                         "#+title: ${title}\n") :unnarrowed t)
-                                     ("p" " Problems" plain "\n* [[id:f23824a1-0515-47c6-b386-21d83a9aec21][PROBLEM]]\n%?\n* SOLVING"
-                                      :icon ("nf-fa-eye" :set "faicon" :color "lcyan")
-                                      :target (file+head "problems/content/%<%Y%m%d%H%M%S>-${slug}.org"
-                                                         "#+title: ${title}\n#+filetags: :Problem:\n") :unnarrowed t))))
+  (org-roam-directory "~/Dropbox/RoamNotes")
+  (org-roam-index-file "~/Dropbox/RoamNotes/index.org")
+  (org-roam-capture-templates
+   `(("d" " Default" plain
+      "%?"
+      :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                         "#+title: ${title}\n")
+      :unnarrowed t)
+     ("p" " Problems" plain
+      "* [[id:f23824a1-0515-47c6-b386-21d83a9aec21][PROBLEM]]\n%?\n* SOLVING"
+      :target (file+head "problems/content/%<%Y%m%d%H%M%S>-${slug}.org"
+                         "#+title: ${title}\n#+filetags: :PROBLEM:\n")
+      :unnarrowed t)
+     ("a", "󰙅 DataStructure" plain
+      "A =${title}= [[id:92421051-83c3-4117-9c25-7f4f9ecf2c0a][Data Structure]] is %?"
+      :target (file+head "%<%Y%m%d%H%M%S>-${slug}.org"
+                         "#+title: ${title}\n#+filetags: :DATASTRUCTURE:\n")
+      :unnarrowed t))))
 
 (use-package! websocket
   :defer t
