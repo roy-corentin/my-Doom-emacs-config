5d4bd0db4ff3ce559efd83d993b4d361a14742d3
roy-corentin <corentin.roy02@laposte.net>
2024-03-31 12:25:43 +0200

    [UP] use-package corfu + lint

--- config.el
+++ config.el
@@ -44,9 +44,12 @@
 (setq! display-line-numbers-type 'visual)
 
 (use-package! corfu
-  :custom
-  (corfu-preselect 'first)
+  :defer t
+  :config
+  (setq corfu-preselect 'first)
   :bind (:map corfu-map
+              ("TAB" . nil)
+              ("<tab>" . nil)
               ("M-TAB" . corfu-complete)
               ("M-<tab>" . corfu-complete)))
 
@@ -462,29 +465,29 @@
   (setq doom-themes-treemacs-theme "doom-colors"))
 
 (setq! treesit-language-source-alist
-  '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-    (c "https://github.com/tree-sitter/tree-sitter-c")
-    (cmake "https://github.com/uyha/tree-sitter-cmake")
-    (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
-    (css "https://github.com/tree-sitter/tree-sitter-css")
-    (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-    (elixir "https://github.com/elixir-lang/tree-sitter-elixir")
-    (go "https://github.com/tree-sitter/tree-sitter-go")
-    (go-mod "https://github.com/camdencheek/tree-sitter-go-mod")
-    (heex "https://github.com/phoenixframework/tree-sitter-heex")
-    (html "https://github.com/tree-sitter/tree-sitter-html")
-    (js . ("https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
-    (json "https://github.com/tree-sitter/tree-sitter-json")
-    (make "https://github.com/alemuller/tree-sitter-make")
-    (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-    (python "https://github.com/tree-sitter/tree-sitter-python")
-    (rust "https://github.com/tree-sitter/tree-sitter-rust")
-    (ruby "https://github.com/tree-sitter/tree-sitter-ruby")
-    (toml "https://github.com/tree-sitter/tree-sitter-toml")
-    (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
-    (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
-    (yaml "https://github.com/ikatyang/tree-sitter-yaml")
-    (latex "https://github.com/latex-lsp/tree-sitter-latex")))
+       '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+         (c "https://github.com/tree-sitter/tree-sitter-c")
+         (cmake "https://github.com/uyha/tree-sitter-cmake")
+         (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+         (css "https://github.com/tree-sitter/tree-sitter-css")
+         (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+         (elixir "https://github.com/elixir-lang/tree-sitter-elixir")
+         (go "https://github.com/tree-sitter/tree-sitter-go")
+         (go-mod "https://github.com/camdencheek/tree-sitter-go-mod")
+         (heex "https://github.com/phoenixframework/tree-sitter-heex")
+         (html "https://github.com/tree-sitter/tree-sitter-html")
+         (js . ("https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
+         (json "https://github.com/tree-sitter/tree-sitter-json")
+         (make "https://github.com/alemuller/tree-sitter-make")
+         (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+         (python "https://github.com/tree-sitter/tree-sitter-python")
+         (rust "https://github.com/tree-sitter/tree-sitter-rust")
+         (ruby "https://github.com/tree-sitter/tree-sitter-ruby")
+         (toml "https://github.com/tree-sitter/tree-sitter-toml")
+         (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src"))
+         (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src"))
+         (yaml "https://github.com/ikatyang/tree-sitter-yaml")
+         (latex "https://github.com/latex-lsp/tree-sitter-latex")))
 
 (use-package! treesit-auto
   :custom
